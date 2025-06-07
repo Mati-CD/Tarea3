02b15217b470a15cc71c73c24c359a706d3bd441
Mati-CD <mcuello2024@udec.cl>
2025-06-06 22:30:54 -0400

    Añadiendo el panel en clase Ventana.java

--- Tarea3/src/main/java/org/example/InterfazVisual/Ventana.java
+++ Tarea3/src/main/java/org/example/InterfazVisual/Ventana.java
@@ -1,4 +1,26 @@
 package org.example.InterfazVisual;
 
-public class Ventana {
+import javax.swing.*;
+
+public class Ventana extends JFrame {
+    public Ventana() {
+        setTitle("Expendedor");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 700);
+        setLocationRelativeTo(null);
+
+        // Crear y agregar el panel principal
+        PanelPrincipal panelPrincipal = new PanelPrincipal();
+        add(panelPrincipal);
+
+        // Configuración adicional de la ventana
+        setResizable(true);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Ventana();
+        });
+    }
 }
